1757da8b18f8bc192214b28ca502a9fbd1009260
mpeoples22 <93021226+mpeoples22@users.noreply.github.com>
2021-11-25 21:54:06 -0600

    refactor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
 var formEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task=type']").value;
-
-  //create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    //create div to hold task info and add to list item
-    var tasksToDoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    //add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
-    tasksToDoEl.appendChild(taskInfoEl);
-//add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
-    rmEl.addEventListener("submit", createTaskHandler);
+  //package up data as an object
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
+  };
+  //send it as an argument to creatTaskEl
+  createTaskEl(taskDataObj);
 };
 
-formEl.addEventListener("click", createTaskHandler);
+var createTaskEl = function (taskDataObj) {
+//create list item
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+ //create div to hold task info and add to list item
+  var tasksToDoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+//add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class= 'task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+//add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+  formEl.addEventListener("click", taskFormHandler);
